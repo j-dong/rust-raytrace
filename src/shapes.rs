03f1b1db1112a6216f3d63be804acb43d3c28927
James <dong.james.sept14@gmail.com>
2015-12-09 14:30:30 -0600

    Work on intersection

--- src/shapes.rs
+++ src/shapes.rs
@@ -2,6 +2,7 @@ extern crate nalgebra as na;
 use na::Inv;
 use na::ToHomogeneous;
 use na::FromHomogeneous;
+use na::Norm;
 
 use std::ops::Mul;
 
@@ -21,20 +22,41 @@ impl Ray {
     fn cast(&self, t: f32) -> Pnt3 {
         self.origin + self.direction * t
     }
+}
 
-    fn transform(&self, mat: Mat4) -> Ray {
-        Ray {
-            origin: FromHomogeneous::<Pnt4>::from(Mul::<Pnt4>::mul(mat, self.origin.to_homogeneous())),
-            direction: FromHomogeneous::<Mat4>::from(&mat) * self.direction,
-        }
-    }
+struct IntersectionResult {
+    t: f32,
+    normal: Vec3,
 }
 
 trait Shape {
-    fn intersect(&self, ray: &Ray);
+    fn intersect(&self, ray: &Ray) -> IntersectionResult;
 }
 
 struct Sphere {
     center: Pnt3,
     radius: f32,
+}
+
+impl Shape for Sphere {
+    fn intersect(&self, ray: &Ray) -> Option<IntersectionResult> {
+        // pt = origin + direction * t
+        // |pt - center|^2 = radius * radius
+        // (ox + dx*t - cx)^2 + ... = radius * radius
+        // (dx^2 + ...) t^2 + 2 ((ox - cx) dx + ...) t + (ox - cx)^2 + ... - radius * radius = 0
+        // so as a quadratic
+        // a = dx^2 + dy^2 + dz^2 = direction.sqnorm()
+        // b = 2 * ((ox - cx) dx + (oy - cy) dy + (oz - cz) dz) = 2 * direction.dot(origin - center)
+        // c = (ox - cx)^2 + (oy - cy)^2 + (oz - cz)^2 - radius * radius = (origin - center).sqnorm() - radius * radius
+        let ominusc = ray.origin - self.center;
+        let a = ray.direction.sqnorm();
+        let b = 2 * ray.direction.dot(ominusc);
+        let c = ominusc.sqnorm() - self.radius * self.radius;
+        // intersects iff b^2 - 4*a*c > 0
+        if (b * b - 4 * a * c > 0) {
+            Some(IntersectionResult { (-b - num::sqrt()) })
+        } else {
+            None
+        }
+    }
 }
