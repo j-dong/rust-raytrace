f8981f274b112c3300c81e5aa3de438b48b7e8eb
James <dong.james.sept14@gmail.com>
2015-12-14 14:27:32 -0600

    Scene classes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod shapes;
 pub mod types;
 pub mod color;
+pub mod scene;
---
+++ src/scene.rs
@@ -0,0 +1,41 @@
+use types::*;
+use shapes::*;
+use color::*;
+
+use std::boxed::Box;
+
+struct Material {
+    diffuse: Color,
+}
+
+struct Object {
+    bounds: Box<Shape>,
+    material: Material,
+}
+
+trait Light {
+    fn light_dir_for(&self, pt: &Pnt3) -> Vec3;
+    fn shadow_ray_for(&self, pt: &Pnt3) -> Ray {
+        Ray { origin: pt.clone(), direction: -self.light_dir_for(pt) }
+    }
+}
+
+struct PointLight {
+    location: Pnt3,
+}
+
+impl Light for PointLight {
+    fn light_dir_for(&self, pt: &Pnt3) -> Vec3 {
+        Vec3::new(pt.x - self.location.x, pt.y - self.location.y, pt.z - self.location.z)
+    }
+}
+
+struct DirectionalLight {
+    direction: Vec3,
+}
+
+impl Light for DirectionalLight {
+    fn light_dir_for(&self, pt: &Pnt3) -> Vec3 {
+        self.direction
+    }
+}
