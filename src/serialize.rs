24afbac0a845ce3e3c64bc43d0299f93aedbd8f6
James Dong <dong.james.sept14@gmail.com>
2016-01-09 10:12:59 -0600

    Use Acceptor to make things cleaner?

--- src/serialize.rs
+++ src/serialize.rs
@@ -5,19 +5,17 @@
 use std::str::{Chars, FromStr};
 use std::iter::{Iterator, Peekable};
 
-#[inline]
-fn skip_while<I: Iterator, F>(iter: &mut Peekable<I>, fun: F) -> bool where F: Fn(&I::Item) -> bool {
-    let mut ret = false;
-    while match iter.peek() {
-        None => false,
-        Some(e) => fun(e)
-    } {
-        ret = true;
-        iter.next();
-    }
-    return ret;
+struct Tracker<I: Iterator> {
+    iter: I,
+    row: usize,
+    col: usize,
 }
 
+struct Acceptor<I: Iterator> {
+    iter: Peekable<I>,
+}
+
+#[must_use = "iterator adaptors are lazy and do nothing unless consumed"]
 struct TakeWhile<'a, I: Iterator + 'a, F: Fn(&I::Item) -> bool> {
     iter: &'a mut Peekable<I>,
     fun: F,
@@ -28,21 +26,57 @@ impl<'a, I: Iterator, F: Fn(&I::Item) -> bool> Iterator for TakeWhile<'a, I, F> 
 
     #[inline]
     fn next(&mut self) -> Option<I::Item> {
-        while let Some(&el) = self.iter.peek() {
-            if (self.fun)(&el) {
-                self.iter.next();
-                return Some(el)
-            } else {
-                return None
-            }
+        if match self.iter.peek() { Some(e) => (self.fun)(e), None => false } {
+            self.iter.next()
+        } else {
+            None
         }
-        None
     }
 }
 
-#[inline]
-fn take_while<'a, I: Iterator, F>(iter: &'a mut Peekable<I>, fun: F) -> TakeWhile<'a, I, F> where F: Fn(&I::Item) -> bool {
-    TakeWhile { iter: iter, fun: fun }
+impl<I: Iterator> Acceptor<I> {
+    #[inline]
+    fn take(&mut self) -> Option<I::Item> { self.iter.next() }
+
+    #[inline]
+    fn peek(&mut self) -> Option<&I::Item> { self.iter.peek() }
+
+    #[inline]
+    fn accept<F>(&mut self, fun: F) -> Option<I::Item> where F: Fn(&I::Item) -> bool {
+        if match self.peek() { Some(e) => fun(e), None => false } {
+            self.take()
+        } else {
+            None
+        }
+    }
+
+    #[inline]
+    fn skip(&mut self) -> bool { self.take().is_some() }
+
+    #[inline]
+    fn skip_if<F>(&mut self, fun: F) -> bool where F: Fn(&I::Item) -> bool {
+        if match self.peek() { Some(e) => fun(e), None => false } {
+            self.skip();
+            true
+        } else {
+            false
+        }
+    }
+
+    #[inline]
+    fn skip_while<F>(&mut self, fun: F) -> bool where F: Fn(&I::Item) -> bool {
+        let mut ret = false;
+        while match self.peek() { Some(e) => fun(e), None => false } {
+            self.skip();
+            ret = true;
+        }
+        return ret;
+    }
+
+    #[inline]
+    fn take_while<F>(&mut self, fun: F) -> TakeWhile<I, F> where F: Fn(&I::Item) -> bool {
+        TakeWhile { iter: &mut self.iter, fun: fun }
+    }
 }
 
 #[derive(Debug)]
@@ -60,42 +94,45 @@ enum Token {
 }
 
 struct Tokenizer<'a> {
-    iter: Peekable<Chars<'a>>,
+    acceptor: Acceptor<Chars<'a>>,
 }
 
 impl<'a> Iterator for Tokenizer<'a> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Token> {
-        skip_while(&mut self.iter, |c: &char| {c.is_whitespace()});
+        self.acceptor.skip_while(|c: &char| {c.is_whitespace()});
         let c = {
-            let ch = self.iter.peek();
-            match ch {
-                Some(c) => *c,
+            match self.acceptor.peek() {
+                Some(&c) => c,
                 None => return None,
             }
         };
         match c {
-            '{' => {self.iter.next(); Some(Token::LBrace)},
-            '}' => {self.iter.next(); Some(Token::RBrace)},
-            '[' => {self.iter.next(); Some(Token::LBracket)},
-            ']' => {self.iter.next(); Some(Token::RBracket)},
-            '(' => {self.iter.next(); Some(Token::LParen)},
-            ')' => {self.iter.next(); Some(Token::RParen)},
-            ':' => {self.iter.next(); Some(Token::Colon)},
-            ',' => {self.iter.next(); Some(Token::Comma)},
-            '#' => {skip_while(&mut self.iter, |c| {(*c) != '\n'}); self.next()},
-            '/' => {self.iter.next(); match self.iter.next() {
-                Some('/') => {skip_while(&mut self.iter, |c| {(*c) != '\n'});},
-                Some('*') => {loop {
-                    skip_while(&mut self.iter, |c| {(*c) != '*'});
-                    self.iter.next();
-                    if let Some('/') = self.iter.next() {break}
-                }},
-                _ => return None
-            }; self.next()},
-            'A' ... 'Z' | 'a' ... 'z' | '_' => Some(Token::Identifier(take_while(&mut self.iter, |c| {match *c {'A' ... 'Z' | 'a' ... 'z' | '0' ... '9' | '_' => true, _ => false}}).collect())),
-            _ => match f32::from_str(&take_while(&mut self.iter, |c| {match *c {'A' ... 'Z' | 'a' ... 'z' | '0' ... '9' | '_' | '.' | '-' | '+' => true, _ => false}}).collect::<String>()) {
+            '{' => {self.acceptor.skip(); Some(Token::LBrace)},
+            '}' => {self.acceptor.skip(); Some(Token::RBrace)},
+            '[' => {self.acceptor.skip(); Some(Token::LBracket)},
+            ']' => {self.acceptor.skip(); Some(Token::RBracket)},
+            '(' => {self.acceptor.skip(); Some(Token::LParen)},
+            ')' => {self.acceptor.skip(); Some(Token::RParen)},
+            ':' => {self.acceptor.skip(); Some(Token::Colon)},
+            ',' => {self.acceptor.skip(); Some(Token::Comma)},
+            '#' => {self.acceptor.skip_while(|c| {(*c) != '\n'}); self.next()},
+            '/' => {
+                self.acceptor.skip(); // discard /
+                match self.acceptor.take() {
+                    Some('/') => {self.acceptor.skip_while(|c| {(*c) != '\n'});},
+                    Some('*') => {loop {
+                        self.acceptor.skip_while(|c| {(*c) != '*'});
+                        self.acceptor.skip(); // discard *
+                        if let Some('/') = self.acceptor.take() {break}
+                    }},
+                    _ => return None
+                }
+                self.next()
+            },
+            'A' ... 'Z' | 'a' ... 'z' | '_' => Some(Token::Identifier(self.acceptor.take_while(|c| {match *c {'A' ... 'Z' | 'a' ... 'z' | '0' ... '9' | '_' => true, _ => false}}).collect())),
+            _ => match f32::from_str(&self.acceptor.take_while(|c| {match *c {'A' ... 'Z' | 'a' ... 'z' | '0' ... '9' | '_' | '.' | '-' | '+' => true, _ => false}}).collect::<String>()) {
                 Ok(e) => Some(Token::Number(e)),
                 Err(_) => None
             },
@@ -108,7 +145,7 @@ pub fn deserialize(text: &str) {
 }
 
 pub fn print_tokens(text: &str) {
-    for tok in (Tokenizer { iter: text.chars().peekable() }) {
+    for tok in (Tokenizer { acceptor: Acceptor { iter: text.chars().peekable() } }) {
         println!("{:?}", tok);
     }
 }
