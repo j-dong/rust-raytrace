29a4dfca11d9fd3d1387d6a00f6409e2bc5b2f67
James Dong <dong.james.sept14@gmail.com>
2016-01-09 13:03:42 -0600

    Make LL1 struct to keep track of location

--- src/serialize.rs
+++ src/serialize.rs
@@ -5,23 +5,104 @@
 use std::str::{Chars, FromStr};
 use std::iter::{Iterator, Peekable};
 
-struct Tracker<I: Iterator> {
-    iter: I,
+#[derive(Copy, Clone, Debug)]
+struct Location {
     row: usize,
     col: usize,
 }
 
-struct Acceptor<I: Iterator> {
-    iter: Peekable<I>,
+impl Location {
+    fn newline(&mut self) {
+        self.row += 1;
+        self.col = 0;
+    }
+
+    fn next(&mut self) {
+        self.col += 1;
+    }
+
+    fn set(&mut self, other: &Location) {
+        self.row = other.row;
+        self.col = other.col;
+    }
+}
+
+trait Processable : Iterator {
+    fn process(it: Option<Self::Item>, loc: &mut Location) -> Option<Self::Item>;
+}
+
+impl<I: Iterator<Item = char>> Processable for I {
+    fn process(it: Option<char>, loc: &mut Location) -> Option<char> {
+        if let Some(c) = it {
+            if c == '\n' {
+                loc.newline();
+            } else {
+                loc.next();
+            }
+        }
+        it
+    }
+}
+
+struct LL1<I: Processable> {
+    iter: I,
+    location: Location,
+    peeked: Option<I::Item>,
+}
+
+impl<I: Processable> LL1<I> {
+    #[inline]
+    fn new(iter: I) -> LL1<I> {
+        LL1 { iter: iter, location: Location { row: 1, col: 0 }, peeked: None }
+    }
+
+    #[inline]
+    fn peek(&mut self) -> Option<&I::Item> {
+        if self.peeked.is_none() {
+            self.peeked = <I as Processable>::process(self.iter.next(), &mut self.location);
+        }
+        match self.peeked {
+            Some(ref value) => Some(value),
+            None => None,
+        }
+    }
+}
+
+impl<I: Processable> Iterator for LL1<I> {
+    type Item = I::Item;
+
+    #[inline]
+    fn next(&mut self) -> Option<I::Item> {
+        match self.peeked {
+            Some(_) => self.peeked.take(),
+            None => <I as Processable>::process(self.iter.next(), &mut self.location),
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (lo, hi) = self.iter.size_hint();
+        if self.peeked.is_some() {
+            let lo = lo.saturating_add(1);
+            let hi = hi.and_then(|x| x.checked_add(1));
+            (lo, hi)
+        } else {
+            (lo, hi)
+        }
+    }
+}
+
+struct Acceptor<I: Processable> {
+    iter: LL1<I>,
 }
 
 #[must_use = "iterator adaptors are lazy and do nothing unless consumed"]
-struct TakeWhile<'a, I: Iterator + 'a, F: Fn(&I::Item) -> bool> {
-    iter: &'a mut Peekable<I>,
+struct TakeWhile<'a, I: Processable + 'a, F: Fn(&I::Item) -> bool> {
+    iter: &'a mut LL1<I>,
     fun: F,
 }
 
-impl<'a, I: Iterator, F: Fn(&I::Item) -> bool> Iterator for TakeWhile<'a, I, F> where I::Item: Copy {
+impl<'a, I: Processable, F: Fn(&I::Item) -> bool> Iterator for TakeWhile<'a, I, F> where I::Item: Copy {
     type Item = I::Item;
 
     #[inline]
@@ -34,7 +115,7 @@ impl<'a, I: Iterator, F: Fn(&I::Item) -> bool> Iterator for TakeWhile<'a, I, F> 
     }
 }
 
-impl<I: Iterator> Acceptor<I> {
+impl<I: Processable> Acceptor<I> {
     #[inline]
     fn take(&mut self) -> Option<I::Item> { self.iter.next() }
 
@@ -145,7 +226,7 @@ pub fn deserialize(text: &str) {
 }
 
 pub fn print_tokens(text: &str) {
-    for tok in (Tokenizer { acceptor: Acceptor { iter: text.chars().peekable() } }) {
+    for tok in (Tokenizer { acceptor: Acceptor { iter: LL1::new(text.chars()) } }) {
         println!("{:?}", tok);
     }
 }
